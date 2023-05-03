9194021b3681f48cc76f552c07c3c82048705e81
Nastenko Peter <source-2306@mail.ru>
2023-05-03 17:14:17 +0300

    1.5 Решена 3 задача.py

--- 1_5/1_5_task_3.py
+++ 1_5/1_5_task_3.py
@@ -1,27 +1,26 @@
+from random import randint
+
+
 class Line:  # линия
     def __init__(self, a, b, c, d):
-        self.a = a
-        self.b = b
-        self.c = c
-        self.d = d
+        self.sp = (a, b)
+        self.ep = (c, d)
 
 
 class Rect:  # прямой
     def __init__(self, a, b, c, d):
-        self.a = a
-        self.b = b
-        self.c = c
-        self.d = d
+        self.sp = (a, b)
+        self.ep = (c, d)
 
 
 class Ellipse:  # еллипс
     def __init__(self, a, b, c, d):
-        self.a = a
-        self.b = b
-        self.c = c
-        self.d = d
+        self.sp = (a, b)
+        self.ep = (c, d)
 
 
-g1 = Line(a, b, c, d)
-g2 = Rect(a, b, c, d)
-g3 = Ellipse(a, b, c, d)
+elements = [(Line, Rect, Ellipse)[randint(0, 2)](1, 2, 3, 4) for n in range(217)]
+
+for obj in elements:
+    if isinstance(obj, Line):
+        obj.sp = obj.ep = 0, 0
